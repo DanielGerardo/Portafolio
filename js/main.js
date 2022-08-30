cb780c5b953553054dc2ac34098b861171dea473
DanielGerardo <danyjakfg@live.com.mx>
2022-08-29 20:36:28 -0500

    Problemas en el Asesor

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const carga = document.querySelector('.carga');
 const App = document.querySelector('.App');
 const body = document.querySelector('body');
-
+const barHabilidades = document.getElementById('barHabilidades');
 setTimeout(() =>{
   body.style.overflowY = 'visible'
   
@@ -12,3 +12,9 @@ setTimeout(() =>{
   carga.style.display = 'none'
   App.style.opacity = 1;
 },3000)
+
+const barScrum = document.getElementById("scrum");
+
+  barScrum.style.animation = 'mostarArriba 5s';
+
+
