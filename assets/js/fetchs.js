7fc8970f6f4af2c922536535eca80515a06cc50f
DanielGerardo <danyjakfg@live.com.mx>
2022-09-29 17:58:16 -0500

    Se implemento Sass , optimizacion de proyectos y modificacion del carusel

---
+++ assets/js/fetchs.js
@@ -0,0 +1,57 @@
+const grid_proyecto = document.getElementById("grid-proyecto");
+
+function addProyecto(item){
+
+    const itemHTML = `
+    <div class="col-12 col-sm-4">
+    <div class="card proyecto">
+    <img
+      src="assets/img/proyectos/${item.src}"
+      class="card-img align-self-center proyectoSoft"
+      alt="${item.src}"
+    />
+    <div class="card-body">
+      <h5 class="card-title">${item.title}</h5>
+      <p class="card-text">${item.desc}
+      </p>
+      <b> Herramientas (${item.herram})</b
+      ><a
+        href="${item.git}"
+        target="_blank"
+        ><svg
+          height="32"
+          class="octicon octicon-mark-github color-text-white"
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="32"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+          ></path></svg
+      ></a>
+    </div>
+  </div>
+  </div>`;
+  grid_proyecto.innerHTML += itemHTML;
+}
+
+function buscarProyectos(){
+    fetch("/assets/archivos/proyectos.json")
+      .then(response => {
+         return response.json();
+      })
+      .then(json =>{
+        //Convierte los datos de los productos en texto
+        proyectos = JSON.stringify(json);
+        //Convierte los datos de los productos en Objeto
+        proyectos = JSON.parse(proyectos);
+    
+        for (let i = 0; i< proyectos.length; i++) {
+            addProyecto(proyectos[i]);
+                      
+        }
+        
+    })};
+buscarProyectos();
---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+
+const carga = document.querySelector('.carga');
+const App = document.querySelector('.App');
+const body = document.querySelector('body');
+const barHabilidades = document.getElementById('barHabilidades');
+setTimeout(() =>{
+  body.style.overflowY = 'visible'
+  
+},3000)
+
+setTimeout(() =>{
+  carga.style.display = 'none'
+  App.style.opacity = 1;
+},3000)
+
+
+const equipo = document.getElementById("equipo");
+const perseverancia = document.getElementById("perseverancia");
+const aprendizaje = document.getElementById("aprendizaje");
+const orientacion = document.getElementById("orientacion");
+const scrum = document.getElementById("scrum");
+const mvc = document.getElementById("mvc");
+const mvp = document.getElementById("mvp");
+
+
+
+function mostrarBarras(){
+  let scrollTop = document.documentElement.scrollTop;
+   
+      if(250 < scrollTop && 800 > scrollTop){
+        equipo.style.animation = 'mostarBarras 3s';
+        perseverancia.style.animation = 'mostarBarras 3.2s';
+        aprendizaje.style.animation = 'mostarBarras 3.4s';
+        orientacion.style.animation = 'mostarBarras 3.6s';
+        scrum.style.animation = 'mostarBarras 3.8s';
+        mvc.style.animation = 'mostarBarras 4s';
+        mvp.style.animation = 'mostarBarras 4.2s';
+      }else{
+        equipo.style.animation = 'none';
+        perseverancia.style.animation = 'none';
+        aprendizaje.style.animation = 'none';
+        orientacion.style.animation = 'none';
+        scrum.style.animation = 'none';
+        mvc.style.animation = 'none';
+        mvp.style.animation = 'none';
+      }
+    
+  }
+  
+  window.addEventListener('scroll',mostrarBarras);
+
+
+
+
+  
